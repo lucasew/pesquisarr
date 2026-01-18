c5b8b599b1379b66371f618b99af284167b25e9e
lucasew <lucas59356@gmail.com>
2026-01-18 16:57:57 -0300

    use linkedom
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,32 +1,18 @@
 import DOMPurify from 'dompurify';
-import { JSDOM } from 'jsdom';
-
-
+import { parseHTML } from 'linkedom';
 
 export function matchFirstGroup(text: string, regex: RegExp): string[] {
-    const matches = [];
-    let match;
-    while ((match = regex.exec(text)) !== null) {
-        matches.push(match[1]);
-    }
-    return matches;
+	const matches = [];
+	let match;
+	while ((match = regex.exec(text)) !== null) {
+		matches.push(match[1]);
+	}
+	return matches;
 }
 
-const window = new JSDOM('').window;
-const purify = DOMPurify(window);
+const { window } = parseHTML('');
+const purify = DOMPurify(window as any);
 
 export function htmlSanitize(str: string): string {
 	return purify.sanitize(str);
 }
-
-
-
-
-
-
-
-
-
-
-
-
